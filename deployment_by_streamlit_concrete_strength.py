71cb5eb07551b91daa3cf6a7a4d532ca6c362995
merrooo <138368815+merrooo@users.noreply.github.com>
2023-12-27 23:31:31 +0200

    Update deployment_by_streamlit_concrete_strength.py

--- deployment_by_streamlit_concrete_strength.py
+++ deployment_by_streamlit_concrete_strength.py
@@ -11,7 +11,6 @@ Original file is located at
 ##CREAT_LOAD_PKL_FILE
 """
 
-!pip install streamlit
 from xgboost import XGBRegressor
 from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import train_test_split
