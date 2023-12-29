5068e28f69440535b0fdff9c6001d051f08b68fe
merrooo <138368815+merrooo@users.noreply.github.com>
2023-12-29 23:45:23 +0200

    Update deploy_1.py

--- deploy_1.py
+++ deploy_1.py
@@ -36,7 +36,7 @@ data_matrix = [['ITEM', 'DESCIBTION'],
 fig = ff.create_table(data_matrix)
 st.plotly_chart(fig)
 page=st.sidebar.selectbox("OPTINOS_FOR_EXPLORING_DATA",("EDA","VISUALIZATION","PREDICTION"))
-if page == "PREDICTION":
+if page == "EDA":
     st.title('EXPLORING_DATA_FRAME!!')
     #------------------------------------------------------------------
     EDA_=st.sidebar.selectbox("EXPLORING_DATA_COLUMNS",("DATA_FARME_","COLUMNS_"))
@@ -106,15 +106,7 @@ if page == "PREDICTION":
          st.write('DONE!!')
     st.balloons()
 elif page =="VISUALIZATION":
-    st.success('ALREADY_GRAPH_VISUALIZED!', icon="✅")
-    st.balloons()
-elif page =="EDA":
-    st.success('ALREADY_DATA_EXPLORED!', icon="✅")
-    st.balloons()
-#------------------------------------------------------------------------------------------------------------
-
-def SHOW_VISU_PAGE():
-    url = 'https://raw.githubusercontent.com/merrooo/ML_DATA/main/concrete_data.csv'
+        url = 'https://raw.githubusercontent.com/merrooo/ML_DATA/main/concrete_data.csv'
     df=pd.read_csv(url)
     x=df.loc[:,df.columns != 'Strength']
     y=df['Strength']
@@ -222,97 +214,9 @@ def SHOW_VISU_PAGE():
     if button_VISU_13:
       fig = px.density_heatmap(df, x="Water", y="Strength", text_auto=True, nbinsx=7, color_continuous_scale='turbid_r', width=686, height=889)
       st.plotly_chart(fig)
-# _______________________________________________________________________________________________________________________________________________________________
-def SHOW_DATA_PAGE():
-
-    url = 'https://raw.githubusercontent.com/merrooo/ML_DATA/main/concrete_data.csv'
-    df=pd.read_csv(url)
-    x=df.loc[:,df.columns != 'Strength']
-    y=df['Strength']
-    #------------------------------------------------------------------
-    st.title('EXPLORING_DATA_FRAME!!')
-    #------------------------------------------------------------------
-    EDA_=st.sidebar.selectbox("EXPLORING_DATA_COLUMNS",("DATA_FARME_","COLUMNS_"))
-
-    if EDA_ == "DATA_FARME_":
-      EXPLORE_DATA_=st.toggle('EXPLORE_DATA',disabled=False)
-      CHECK_NULL_=st.toggle('CHECK_NULL',disabled=False)
-      DESCRIBE_=st.toggle('DESCRIBE',disabled=False)
-      COLUMNS_=st.toggle('COLUMNS',disabled=False)
-      DUPLICATED_=st.toggle('DUPLICATED',disabled=False)
-      MAX_=st.toggle('MAXIMUM_VALUES_FEATURES',disabled=False)
-      HEAD_=st.toggle('DATA_HEAD',disabled=False)
-      if EXPLORE_DATA_:
-        st.write('EXPLORING_DATA_FRAM_OF_CONCRETE!!')
-        st.dataframe(df)
-        st.write('DONE!!')
-      elif MAX_:
-        st.write('MAXIMUM_VALUES_FEATURES!!')
-        data_matrix = [['ITEM', 'DESCIBTION'],
-               ['Cement_KG IN M3',540.0],
-               ['Blast Furnace Slag_KG IN M3', 359.4],
-               ['Fly Ash_KG IN M3', 200.1],
-               ['Water_KG IN M3', 247.0],
-               ['Superplasticizer IN M3', 32.2],
-               ['Coarse Aggregate_KG IN M3', 1145.0],
-               ['Fine Aggregate_KG IN M3', 992.6],
-               ['Age_Day (1~365)', 365],
-               ['Strength_MPa', 82.6]]
-        fig = ff.create_table(data_matrix)
-        st.plotly_chart(fig)
-        st.write('DONE!!')
-      elif HEAD_:
-        st.write('DATA_HEAD!!')
-        st.dataframe(df.head())
-        st.write('DONE!!')
-      elif CHECK_NULL_:
-        st.write('EXPLORING_NULL!!')
-        st.dataframe(df.isnull().sum())
-        st.write('DONE!!')
-      elif DESCRIBE_:
-        st.write('DESCRIPE_OF_DATA!!')
-        st.dataframe(df.describe())
-        st.write('DONE!!')
-      elif COLUMNS_:
-        st.write('EXPLORE_HOW_MANY_FEATURES!!')
-        st.dataframe(df.columns)
-        st.write('DONE!!')
-      elif DUPLICATED_:
-        st.write('CHECK_DUPLICATED_OF_DATA!!')
-        st.dataframe(df.duplicated().sum())
-        st.write('DONE!!')
-    elif EDA_ == "COLUMNS_":
-      st.write('VALUE_COUNTS_FOR_OUT_PUT[STRENGTH]!!')
-      VALUE_COUNTS_=st.toggle('VALUE_COUNTS',disabled=False)
-      UNIQUE_=st.toggle('UNIQUE',disabled=False)
-      if VALUE_COUNTS_:
-        st.write('CHECK_FOR_OUTPUT_[STRENGTH]!!')
-        Strength_=st.toggle('Strength',disabled=False)
-        if Strength_:
-         st.dataframe(df['Strength'].value_counts())
-         st.write('DONE!!')
-      elif UNIQUE_:
-        st.write('CHECK_FOR_OUTPUT_[STRENGTH]!!')
-        Strength_=st.toggle('Strength',disabled=False)
-        if Strength_:
-         st.dataframe(df['Strength'].unique())
-         st.write('DONE!!')
-# _______________________________________________________________________________________________________________________________________________________________
-import streamlit as st
-from sklearn.model_selection import GridSearchCV
-from sklearn.model_selection import train_test_split
-from sklearn.model_selection import cross_val_score,TimeSeriesSplit
-from sklearn.metrics import mean_absolute_error , mean_squared_error ,r2_score
-from xgboost import XGBRegressor
-
-def load_model():
-  with open('amir.pkl','rb') as file:
-    data=pickle.load(file)
-  return data
-data=load_model()
-Strength_=data['model']
-
-def SHOW_PREDICTION_PAGE():
+    st.success('ALREADY_GRAPH_VISUALIZED!', icon="✅")
+    st.balloons()
+elif page =="PREDICTION":
   st.title("SOFTWARE_DEVELOPER_PREDICTION")
   st.write("""###WE_NEED_SOME_INFORMATION_TO_PREDICT_THE STRENGTH OF CONCRETE""")
   #------------------------------------------------------------------
@@ -408,3 +312,5 @@ def SHOW_PREDICTION_PAGE():
     st.plotly_chart(fig)
     #--------------------------------------------------------------------------
     st.success('ALREADY_MODEL_PREDICTED!', icon="✅")
+    st.success('ALREADY_DATA_EXPLORED!', icon="✅")
+    st.balloons()
