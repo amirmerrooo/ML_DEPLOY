583020382ef492e8dda3bd57b855dac4eab166e5
merrooo <138368815+merrooo@users.noreply.github.com>
2023-12-30 00:37:35 +0200

    Update deploy_1.py

--- deploy_1.py
+++ deploy_1.py
@@ -191,7 +191,6 @@ elif page =="VISUALIZATION":
     if button_VISU_8:
       fig = px.imshow(DATA_FRAME('df'), text_auto=True, aspect="auto")
       st.plotly_chart(fig)
-      st.pyplot(DATA_FRAME('df').hist(figsize = (13,10)))
    #------------------------------------------------------------------
     st.write("HEAT_MAP(CEMENT_STRENGTH)")
     button_VISU_9=st.button("HEAT_MAP(CEMENT_STRENGTH)",type="primary")
