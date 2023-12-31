c2dfb1f7b37d4bbd21dbda48df86f56ec2595990
merrooo <138368815+merrooo@users.noreply.github.com>
2023-12-31 20:09:55 +0200

    Update deploy_1.py

--- deploy_1.py
+++ deploy_1.py
@@ -13,41 +13,11 @@ from xgboost import XGBRegressor
 from sklearn.metrics import mean_absolute_error , mean_squared_error ,r2_score
 from sklearn.model_selection import cross_val_score,TimeSeriesSplit
 
-st.header('SOFTWARE_DEVELOPER_AI', divider='red')
-st.write("Concrete is the most important material in civil engineering.The concrete compressive strength is a highly nonlinear function of age andingredients. These ingredients include cement, blast furnace slag, fly ash, water, superplasticizer, coarse aggregate, and fine aggregate.So this the prediction for the strength regarding to the dependent features")
-st.write("-- Input Variable --")
-st.write("Cement (component 1) -- quantitative -- kg in a m3 mixture")
-st.write("Blast Furnace Slag (component 2) -- quantitative -- kg in a m3 mixture")
-st.write("Fly Ash (component 3) -- quantitative -- kg in a m3 mixture")
-st.write("Water (component 4) -- quantitative -- kg in a m3 mixture")
-st.write("Superplasticizer (component 5) -- quantitative -- kg in a m3 mixture")
-st.write("Coarse Aggregate (component 6) -- quantitative -- kg in a m3 mixture")
-st.write("Fine Aggregate (component 7) -- quantitative -- kg in a m3 mixture")
-st.write("Age -- quantitative -- Day (1~365)")
-st.write("-- Output Variable --")
-st.write("Concrete compressive strength -- quantitative -- MPa")
-st.header('_AMIR_ is :blue[cool] :sunglasses:')
-st.header("CONCRETE_DATA_SET")
-st.image("https://media.istockphoto.com/id/692096736/photo/concrete-pouring-during-commercial-concreting-floors-of-building.jpg?s=1024x1024&w=is&k=20&c=XYYH7UhgqsMmwGBWO6UJsxaSgjxNDuQO8i7N27nwRlk=", width=200)
-st.write('-------------------------------CONCLUSOR---------------------------------')
-data_matrix = [['ITEM', 'DESCIBTION'],
-               ['DATA_SET', 'CONCRETE_STRENGTH'],
-               ['CROSS_VALIDATION', 'TimeSeriesSplit'],
-               ['MODEL', 'XGBOOST_REGRESSION'],
-               ['SCORE_TRAIN-%', 99.17],
-               ['SCORE_TEST-%', 93.86]]
-fig = ff.create_table(data_matrix)
-st.plotly_chart(fig)
-
-page=st.sidebar.selectbox("OPTINOS_FOR_EXPLORING_DATA",("EDA","VISUALIZATION","PREDICTION"))
-
 def DATA_FRAME(df):
   url_1= 'https://raw.githubusercontent.com/merrooo/ML_DATA/main/concrete_data.csv'
   df=pd.read_csv(url_1)
   return df
-#_______________________________________________________________________________________________________________________________________________________________
 
-# page=st.sidebar.selectbox("OPTINOS_FOR_EXPLORING_DATA",("EDA","VISUALIZATION","PREDICTION"))
 if page == "EDA":
     st.title('EXPLORING_DATA_FRAME!!')
     #------------------------------------------------------------------
@@ -296,3 +266,31 @@ elif page =="PREDICTION":
     #--------------------------------------------------------------------------
     st.success('ALREADY_MODEL_PREDICTED!', icon="✅")
     st.balloons()
+else:
+  st.header('SOFTWARE_DEVELOPER_AI', divider='red')
+  st.write("Concrete is the most important material in civil engineering.The concrete compressive strength is a highly nonlinear function of age andingredients. These ingredients include cement, blast furnace slag, fly ash, water, superplasticizer, coarse aggregate, and fine aggregate.So this the prediction for the strength regarding to the dependent features")
+  st.write("-- Input Variable --")
+  st.write("Cement (component 1) -- quantitative -- kg in a m3 mixture")
+  st.write("Blast Furnace Slag (component 2) -- quantitative -- kg in a m3 mixture")
+  st.write("Fly Ash (component 3) -- quantitative -- kg in a m3 mixture")
+  st.write("Water (component 4) -- quantitative -- kg in a m3 mixture")
+  st.write("Superplasticizer (component 5) -- quantitative -- kg in a m3 mixture")
+  st.write("Coarse Aggregate (component 6) -- quantitative -- kg in a m3 mixture")
+  st.write("Fine Aggregate (component 7) -- quantitative -- kg in a m3 mixture")
+  st.write("Age -- quantitative -- Day (1~365)")
+  st.write("-- Output Variable --")
+  st.write("Concrete compressive strength -- quantitative -- MPa")
+  st.header('_AMIR_ is :blue[cool] :sunglasses:')
+  st.header("CONCRETE_DATA_SET")
+  st.image("https://media.istockphoto.com/id/692096736/photo/concrete-pouring-during-commercial-concreting-floors-of-building.jpg?s=1024x1024&w=is&k=20&c=XYYH7UhgqsMmwGBWO6UJsxaSgjxNDuQO8i7N27nwRlk=", width=200)
+  st.write('-------------------------------CONCLUSOR---------------------------------')
+  data_matrix = [['ITEM', 'DESCIBTION'],
+               ['DATA_SET', 'CONCRETE_STRENGTH'],
+               ['CROSS_VALIDATION', 'TimeSeriesSplit'],
+               ['MODEL', 'XGBOOST_REGRESSION'],
+               ['SCORE_TRAIN-%', 99.17],
+               ['SCORE_TEST-%', 93.86]]
+  fig = ff.create_table(data_matrix)
+  st.plotly_chart(fig)
+
+  page=st.sidebar.selectbox("OPTINOS_FOR_EXPLORING_DATA",("-*15","EDA","VISUALIZATION","PREDICTION"))  
