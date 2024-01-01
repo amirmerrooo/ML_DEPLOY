8f689c83f1821f99b275b0aea5c99e0ed642fd8c
merrooo <138368815+merrooo@users.noreply.github.com>
2024-01-02 00:59:32 +0200

    Update deploy_1.py

--- deploy_1.py
+++ deploy_1.py
@@ -222,7 +222,7 @@ elif page =="- PREDICTION -":
       Coarse_Aggregate_=st.number_input("Coarse_Aggregate_kg in a m3")
       Fine_Aggregate_=st.number_input("Fine_Aggregate_kg in a m3")
       Age_=st.number_input("Age_Day (1~365)")
-      return float(Cement_),float(Blast_Furnace_Slag_),float(Fly_Ash_),float(Water_),float(Superplasticizer_),float(Coarse_Aggregate_),float(Fine_Aggregate_),float(Age_)
+    return (np.array([Cement_,Blast_Furnace_Slag_,Fly_Ash_,Water_,Superplasticizer_,Coarse_Aggregate_,Fine_Aggregate_,Age_]))
    train()
    submitted = st.form_submit_button("SUBMIT")
   ok=st.button("PREDICTION_STRENGTH_CONCRETE")
