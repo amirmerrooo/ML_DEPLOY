1174dc63b7eaa2ddc096f368be1155c6cfe849c9
merrooo <138368815+merrooo@users.noreply.github.com>
2024-01-01 21:56:15 +0200

    Update deploy_1.py

--- deploy_1.py
+++ deploy_1.py
@@ -246,9 +246,18 @@ elif page =="- PREDICTION -":
     my_bar.empty()
     #--------------------------------------------------------------------------
     st.subheader(f" THE_ESTIMATED_STRENGTH_IS :- \n[{Strength_[0]:.2f}] MPa")
-    data_1 = {
-        'Cement_':np.array(Cement_)}
-    new_data=pd.dataframe(data_1)
+    def date_head():
+      data_1 = { 'Cement_':np.array([Cement_]),
+                'Blast_Furnace_Slag_':np.array([Blast_Furnace_Slag_]),
+                'Fly_Ash_':np.array([Fly_Ash_]),
+                'Water_':np.array([Water_]),
+                'Superplasticizer_':np.array([Superplasticizer_]),
+                'Coarse_Aggregate_':np.array([Coarse_Aggregate_]),
+                'Fine_Aggregate_':np.array([Fine_Aggregate_]),
+                'Age_':np.array([Age_]),
+                'Strength_':np.array([Strength_])}
+      return data_1
+    new_data=pd.DataFrame(date_head())
     st.dataframe(new_data)
     # df.to_excel('NEW_PREDICTION_DATA.xlsx', index=False)
     st.write('------------------------------ACCURACY_TRAIN-----------------------------')
